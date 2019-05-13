636098833ab906dce382de08c7994a547dbf9e66
8023ljq <809395347@qq.com>
2019-05-13 18:30:16 +0800

    修改

--- DapperAdmin/DapperAdmin/App_Start/RouteConfig.cs
+++ DapperAdmin/DapperAdmin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DapperAdmin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Text", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
