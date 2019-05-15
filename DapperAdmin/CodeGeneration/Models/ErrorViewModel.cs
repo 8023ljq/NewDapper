93ddebeed972fbc4257ed2d04eec5d3da79eb53f
8023ljq <809395347@qq.com>
2019-05-15 17:23:50 +0800

    添加Code项目

---
+++ DapperAdmin/CodeGeneration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeGeneration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
