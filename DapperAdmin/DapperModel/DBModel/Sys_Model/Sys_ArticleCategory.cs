d83f6693628e61f60175516f5fa0d361a1721fbb
8023ljq <809395347@qq.com>
2019-05-15 18:10:14 +0800

    修改

--- DapperAdmin/DapperModel/DBModel/Sys_Model/Sys_ArticleCategory.cs
+++ DapperAdmin/DapperModel/DBModel/Sys_Model/Sys_ArticleCategory.cs
@@ -1,68 +1,68 @@
 using System;
-                             using System.Text;
-                             using System.Collections.Generic;
-                             using System.Data;
-                             using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Data;
 
-                             namespace DapperModel.DBModel.SysModel{
-                                /// <summary>
-                                /// 文章类别
-                                /// </summary>
-                                public class Sys_ArticleCategory
-                                {
-                            /// <summary>
-                            /// 主键
-                            /// </summary>
-                            public  string  Id { get; set; }
+namespace DapperModel.DBModel.SysModel
+{
+    /// <summary>
+    /// 文章类别
+    /// </summary>
+    public class Sys_ArticleCategory
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public string Id { get; set; }
 
-                            /// <summary>
-                            /// 分类标题
-                            /// </summary>
-                            public  string  Title { get; set; }
+        /// <summary>
+        /// 分类标题
+        /// </summary>
+        public string Title { get; set; }
 
-                            /// <summary>
-                            /// 父分类ID
-                            /// </summary>
-                            public  string  ParentId { get; set; }
+        /// <summary>
+        /// 父分类ID
+        /// </summary>
+        public string ParentId { get; set; }
 
-                            /// <summary>
-                            /// 类别ID列表(逗号分隔开)
-                            /// </summary>
-                            public  string  ClassList { get; set; }
+        /// <summary>
+        /// 类别ID列表(逗号分隔开)
+        /// </summary>
+        public string ClassList { get; set; }
 
-                            /// <summary>
-                            /// 类别深度
-                            /// </summary>
-                            public  int  ClassLayer { get; set; }
+        /// <summary>
+        /// 类别深度
+        /// </summary>
+        public int ClassLayer { get; set; }
 
-                            /// <summary>
-                            /// 排序
-                            /// </summary>
-                            public  int  Sort { get; set; }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int Sort { get; set; }
 
-                            /// <summary>
-                            /// 分类图标
-                            /// </summary>
-                            public  string  ImageUrl { get; set; }
+        /// <summary>
+        /// 分类图标
+        /// </summary>
+        public string ImageUrl { get; set; }
 
-                            /// <summary>
-                            /// 分类SEO标题
-                            /// </summary>
-                            public  string  SeoTitle { get; set; }
+        /// <summary>
+        /// 分类SEO标题
+        /// </summary>
+        public string SeoTitle { get; set; }
 
-                            /// <summary>
-                            /// 分类SEO关键字
-                            /// </summary>
-                            public  string  SeoKeywords { get; set; }
+        /// <summary>
+        /// 分类SEO关键字
+        /// </summary>
+        public string SeoKeywords { get; set; }
 
-                            /// <summary>
-                            /// 分类SEO描述
-                            /// </summary>
-                            public  string  SeoDescription { get; set; }
+        /// <summary>
+        /// 分类SEO描述
+        /// </summary>
+        public string SeoDescription { get; set; }
 
-                            /// <summary>
-                            /// 是否删除
-                            /// </summary>
-                            public  bool  IsDeleted { get; set; }
- }
-                           }
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
--- DapperAdmin/DapperModel/DBModel/Sys_Model/Sys_Menu.cs
+++ DapperAdmin/DapperModel/DBModel/Sys_Model/Sys_Menu.cs
@@ -1,93 +1,93 @@
 using System;
-                             using System.Text;
-                             using System.Collections.Generic;
-                             using System.Data;
-                             using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Data;
 
-                             namespace DapperModel.DBModel.SysModel{
-                                /// <summary>
-                                /// 系统菜单
-                                /// </summary>
-                                public class Sys_Menu
-                                {
-                            /// <summary>
-                            /// 主键Id
-                            /// </summary>
-                            public  string  Id { get; set; }
+namespace DapperModel.DBModel.SysModel
+{
+    /// <summary>
+    /// 系统菜单
+    /// </summary>
+    public class Sys_Menu
+    {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        public string Id { get; set; }
 
-                            /// <summary>
-                            /// 父级Id
-                            /// </summary>
-                            public  string  ParentId { get; set; }
+        /// <summary>
+        /// 父级Id
+        /// </summary>
+        public string ParentId { get; set; }
 
-                            /// <summary>
-                            /// 菜单名称
-                            /// </summary>
-                            public  string  FullName { get; set; }
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string FullName { get; set; }
 
-                            /// <summary>
-                            /// 菜单层级
-                            /// </summary>
-                            public  int  Layers { get; set; }
+        /// <summary>
+        /// 菜单层级
+        /// </summary>
+        public int Layers { get; set; }
 
-                            /// <summary>
-                            /// 图标地址
-                            /// </summary>
-                            public  string  IconUrl { get; set; }
+        /// <summary>
+        /// 图标地址
+        /// </summary>
+        public string IconUrl { get; set; }
 
-                            /// <summary>
-                            /// 连接地址
-                            /// </summary>
-                            public  string  AddressUrl { get; set; }
+        /// <summary>
+        /// 连接地址
+        /// </summary>
+        public string AddressUrl { get; set; }
 
-                            /// <summary>
-                            /// 排序字段
-                            /// </summary>
-                            public  int  Sort { get; set; }
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public int Sort { get; set; }
 
-                            /// <summary>
-                            /// 操作权限
-                            /// </summary>
-                            public  string  Purview { get; set; }
+        /// <summary>
+        /// 操作权限
+        /// </summary>
+        public string Purview { get; set; }
 
-                            /// <summary>
-                            /// 是否显示
-                            /// </summary>
-                            public  bool  IsShow { get; set; }
+        /// <summary>
+        /// 是否显示
+        /// </summary>
+        public bool IsShow { get; set; }
 
-                            /// <summary>
-                            /// 是否默认
-                            /// </summary>
-                            public  bool  IsDefault { get; set; }
+        /// <summary>
+        /// 是否默认
+        /// </summary>
+        public bool IsDefault { get; set; }
 
-                            /// <summary>
-                            /// 添加人
-                            /// </summary>
-                            public  string  AddUserId { get; set; }
+        /// <summary>
+        /// 添加人
+        /// </summary>
+        public string AddUserId { get; set; }
 
-                            /// <summary>
-                            /// 添加时间
-                            /// </summary>
-                            public  DateTime  AddTime { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime AddTime { get; set; }
 
-                            /// <summary>
-                            /// 修改人
-                            /// </summary>
-                            public  string  UpdateUserId { get; set; }
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        public string UpdateUserId { get; set; }
 
-                            /// <summary>
-                            /// 修改时间
-                            /// </summary>
-                            public  DateTime  UpdateTime { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
 
-                            /// <summary>
-                            /// 是否删除(0:否1:是)
-                            /// </summary>
-                            public  bool  IsDelete { get; set; }
+        /// <summary>
+        /// 是否删除(0:否1:是)
+        /// </summary>
+        public bool IsDelete { get; set; }
 
-                            /// <summary>
-                            /// 备注
-                            /// </summary>
-                            public  string  Remarks { get; set; }
- }
-                           }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remarks { get; set; }
+    }
+}
