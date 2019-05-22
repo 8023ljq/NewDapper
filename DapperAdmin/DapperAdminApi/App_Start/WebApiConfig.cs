e7d3fe7d00f2737c2b8bfd427dccd139749a7a87
8023ljq <809395347@qq.com>
2019-05-22 17:40:54 +0800

    添加AdminApi

---
+++ DapperAdmin/DapperAdminApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DapperAdminApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 配置和服务
+
+            // Web API 配置和服务
+            //跨域配置
+            var allowOrigins = ConfigurationManager.AppSettings["cors_allowOrigins"];
+            var allowHeaders = ConfigurationManager.AppSettings["cors_allowHeaders"];
+            var allowMethods = ConfigurationManager.AppSettings["cors_allowMethods"];
+            var globalCors = new EnableCorsAttribute(allowOrigins, allowHeaders, allowMethods)
+            {
+                SupportsCredentials = true,
+            };
+            config.EnableCors(globalCors);
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ DapperAdmin/DapperAdminApi/Controllers/SysControllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DapperAdminApi.Controllers.SysControllers
+{
+    /// <summary>
+    /// Author：Geek Dog  Content：登录接口 AddTime：2019-5-22 15:33:13  
+    /// </summary>
+    [RoutePrefix("v1/api/login")]
+    public class LoginController : ApiController
+    {
+        /// <summary>
+        /// Author：Geek Dog  Content：后台登录 AddTime：2019-5-22 15:32:55  
+        /// </summary>
+        /// <param name="dynamic"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("loginact")]
+        public IHttpActionResult LoginAct()
+        {
+            return Ok();
+        }
+    }
+}
