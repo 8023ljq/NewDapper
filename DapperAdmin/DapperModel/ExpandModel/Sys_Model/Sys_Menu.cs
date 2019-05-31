681b574a5d4dd91d015de0d53b5bd353654f55bc
8023ljq <809395347@qq.com>
2019-05-31 18:13:11 +0800

    添加

---
+++ DapperAdmin/DapperModel/ExpandModel/Sys_Model/Sys_Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperModel
+{
+    public partial class Sys_Menu
+    {
+        /// <summary>
+        /// 下级菜单
+        /// </summary>
+        [NotMapped]
+        public List<Sys_Menu> LowerMenuList { get; set; }
+    }
+}
