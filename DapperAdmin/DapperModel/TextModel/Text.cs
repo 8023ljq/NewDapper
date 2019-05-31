e3b1eb5b4cc1d18d6ee7e64d92c9a7e130b82d82
8023ljq <809395347@qq.com>
2019-05-31 09:28:01 +0800

    添加

---
+++ DapperAdmin/DapperModel/TextModel/Text.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperModel.TextModel
+{
+    public class Text
+    {
+        public string title { get; set; }
+
+        public string username { get; set; }
+    }
+}
