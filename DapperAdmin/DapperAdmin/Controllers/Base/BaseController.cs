5d080d83f409715ca46b32ac76ea7ac32ef059e7
8023ljq <809395347@qq.com>
2019-05-31 09:22:54 +0800

    添加

---
+++ DapperAdmin/DapperAdmin/Controllers/Base/BaseController.cs
@@ -0,0 +1,52 @@
+﻿using DapperCommonMethod.CommonEnum;
+using DapperCommonMethod.CommonJson;
+using DapperCommonMethod.CommonModel;
+using System.Web.Mvc;
+
+namespace DapperAdmin.Controllers
+{
+    public class BaseController : Controller
+    {
+        
+        /// <summary>
+        /// 普通返回成功
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        protected virtual ActionResult Success(string message)
+        {
+            return Content(new AjaxResult { ResultCode = (int)ResultTypeEnum.success, ResultMsg = message }.ToJson());
+        }
+
+        /// <summary>
+        /// 带参数的返回成功
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        protected virtual ActionResult Success(string message, object data)
+        {
+            return Content(new AjaxResult { ResultCode = (int)ResultTypeEnum.success, ResultMsg = message, ResultData = data }.ToJson());
+        }
+        
+        /// <summary>
+        /// 返回失败
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        protected virtual ActionResult Error(string message)
+        {
+            return Content(new AjaxResult { ResultCode = (int)ResultTypeEnum.error, ResultMsg = message }.ToJson());
+        }
+
+        /// <summary>
+        /// 返回警告
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        protected virtual ActionResult Warning(string message)
+        {
+            return Content(new AjaxResult { ResultCode = (int)ResultTypeEnum.warning, ResultMsg = message }.ToJson());
+        }
+    }
+}
---
+++ DapperAdmin/DapperAdmin/Package/js/common/common.js
@@ -0,0 +1,56 @@
+﻿layui.use(['form', 'layedit', 'laydate'], function () {
+    var form = layui.form
+        , layer = layui.layer;
+    //监听提交
+    form.on('submit(demo1)', function (data) {
+        var obj = $(this);
+        obj.text("登录中...").attr("disabled", "disabled").addClass("layui-disabled");
+        $.ajax({
+            type: 'POST',
+            url: '/Home/IndexAct/',
+            data: data.field,
+            dataType: "json",
+            success: function (res) {//res为相应体,function为回调函数
+                $.LayerMsg(res.ResultMsg, res.ResultCode);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                layer.alert('操作失败！！！' + XMLHttpRequest.status + "|" + XMLHttpRequest.readyState + "|" + textStatus, { icon: 5 });
+            },
+            complete: function () {
+                obj.text("登录").removeAttr("disabled").removeClass("layui-disabled");
+
+            }
+        });
+        return false;
+    });
+})
+
+//弹窗提示 ResultMsg:提示内容 ResultType:提示方式
+$.LayerMsg = function (ResultMsg, ResultType) {
+    if (ResultType >= 0) {
+        var icon = "";
+        var str = "";
+        switch (ResultType) {
+            case 0://正常
+                icon = "fa fa-check-circle";
+                str = "success";
+                break;
+            case 1://错误
+                icon = "fa fa-times-circle";
+                str = "error";
+                break;
+            case 2://警告
+                icon = "fa fa-exclamation-circle";
+                str = "warning";
+                break;
+            default:
+                icon = "fa fa-times-circle";
+                str = "error";
+                break;
+        }
+    }
+    top.layer.msg(ResultMsg, { icon: 0, time: 1000, shift: 1, shade: [0.3, '#000'] });
+    top.$(".layui-layer-msg").css("border-radius", "5px")
+    top.$(".layui-layer-msg").children("div").addClass('layui-layer-msg-' + str);
+    top.$(".layui-layer-msg").children("div").children("i").removeClass('layui-layer-ico layui-layer-ico').addClass(icon);
+}
