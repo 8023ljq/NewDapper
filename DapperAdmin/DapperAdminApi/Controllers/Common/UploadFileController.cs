1b70561ef0f5997f37454ab50548c95d4cd3aca8
8023ljq <809395347@qq.com>
2019-06-26 18:09:00 +0800

    添加控制器

---
+++ DapperAdmin/DapperAdminApi/Controllers/Common/UploadFileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DapperAdminApi.Controllers.Common
+{
+    /// <summary>
+    /// Author：Geek Dog  Content： AddTime：2019-6-26 10:16:36  
+    /// </summary>
+    public class UploadFileController : ApiController
+    {
+    }
+}
