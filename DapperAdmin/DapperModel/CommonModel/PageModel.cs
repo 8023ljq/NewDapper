0fe7de2969477ca254287cd883af7e9835fcc93a
8023ljq <809395347@qq.com>
2019-07-01 23:47:46 +0800

    添加实体类

---
+++ DapperAdmin/DapperModel/CommonModel/PageModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperModel.CommonModel
+{
+    /// <summary>
+    /// 分页查询实体类
+    /// </summary>
+    public class PageModel
+    {
+        /// <summary>
+        /// 每页大小
+        /// </summary>
+        public int pageSize { get; set; }
+
+        /// <summary>
+        /// 第几页
+        /// </summary>
+        public int curPage { get; set; }
+
+        /// <summary>
+        /// 总行数
+        /// </summary>
+        public int count { get; set; }
+    }
+}
---
+++ DapperAdmin/DapperModel/CommonModel/SelectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperModel.CommonModel
+{
+    /// <summary>
+    /// 查询条件实体类（带分页）
+    /// </summary>
+    public class SelectModel : PageModel
+    {
+        /// <summary>
+        /// 关键字查询
+        /// </summary>
+        public string Keyword { get; set; }
+    }
+}
