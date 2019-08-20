77d42cc6cef088c2f8f481c3a9b559c6c7af52a1
8023ljq <809395347@qq.com>
2019-08-20 17:41:00 +0800

    检查未提交的

---
+++ DapperAdmin/DapperCommonMethod/CommonMethod/ReturnHelpMethod.cs
@@ -0,0 +1,50 @@
+﻿using DapperCommonMethod.CommonConfig;
+using DapperCommonMethod.CommonJson;
+using DapperCommonMethod.CommonModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperCommonMethod.CommonMethod
+{
+    /// <summary>
+    /// 处理返回参数
+    /// </summary>
+    public class ReturnHelpMethod
+    {
+        /// <summary>
+        /// Author：Geek Dog  Content：普通请求成功 AddTime：2019-1-8 14:25:30  
+        /// </summary>
+        /// <param name="enumType">枚举类型</param>
+        /// <param name="Model">返回参数</param>
+        /// <returns></returns>
+        public static ResultMsg ReturnSuccess(object ResultCode, object Model = null)
+        {
+            ResultMsg msg = new ResultMsg();
+            msg.ResultCode = 200;
+            msg.ResultMsgs = JosnHelp.Readjson(ResultCode.ToString(), LanguageConfig.CN);
+            msg.ResultData = Model;
+            return msg;
+        }
+
+        /// <summary>
+        /// Author：Geek Dog  Content：普通请求失败 AddTime：2019-1-8 14:29:48  
+        /// </summary>
+        /// <param name="enumType"></param>
+        /// <param name="Model"></param>
+        /// <param name="Errormsg"></param>
+        /// <returns></returns>
+        public static ResultMsg ReturnError(object ResultCode, dynamic Model = null)
+        {
+            ResultMsg msg = new ResultMsg();
+            msg.ResultCode = ResultCode;
+            msg.ResultMsgs = JosnHelp.Readjson(ResultCode.ToString(), LanguageConfig.CN);
+            msg.ResultData = Model;
+            return msg;
+        }
+
+    }
+}
+
---
+++ DapperAdmin/DapperModel/ViewModel/RoleSelectViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperModel.ViewModel
+{
+    /// <summary>
+    /// 角色列表下拉框展示数据类
+    /// </summary>
+    public class RoleSelectViewModel
+    {
+        /// <summary>
+        /// 对应主键ID
+        /// </summary>
+        public string value { get; set; }
+
+        /// <summary>
+        /// 对应显示数据
+        /// </summary>
+        public string label { get; set; }
+
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool disabled { get; set; }
+    }
+}
