50b7be6687c9e641a32e0a69e3afae0a1ca43905
8023ljq <809395347@qq.com>
2019-09-12 17:44:37 +0800

    修改

--- DapperAdmin/DapperCommonMethod/CommonMethod/WriteLogMethod.cs
+++ DapperAdmin/DapperCommonMethod/CommonMethod/WriteLogMethod.cs
@@ -44,14 +44,25 @@ namespace DapperCommonMethod.CommonMethod
                         DateTime.Now.Day + "_Log.txt";
                 }
                 //把异常信息输出到文件，因为异常文件由这几部分组成，这样就不用我们自己复制到文档中了
-                StreamWriter fs = new StreamWriter(LogAddress, true);
-                fs.WriteLine("当前时间：" + DateTime.Now.ToString());
-                fs.WriteLine("异常信息：" + ex.Message);
-                fs.WriteLine("异常对象：" + ex.Source);
-                fs.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
-                fs.WriteLine("触发方法：" + ex.TargetSite);
-                fs.WriteLine();
-                fs.Close();
+
+                using (StreamWriter fs = new StreamWriter(LogAddress, true))
+                {
+                    fs.WriteLine("当前时间：" + DateTime.Now.ToString());
+                    fs.WriteLine("异常信息：" + ex.Message);
+                    fs.WriteLine("异常对象：" + ex.Source);
+                    fs.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
+                    fs.WriteLine("触发方法：" + ex.TargetSite);
+                    fs.WriteLine();
+                }
+
+                //StreamWriter fs = new StreamWriter(LogAddress, true);
+                //fs.WriteLine("当前时间：" + DateTime.Now.ToString());
+                //fs.WriteLine("异常信息：" + ex.Message);
+                //fs.WriteLine("异常对象：" + ex.Source);
+                //fs.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
+                //fs.WriteLine("触发方法：" + ex.TargetSite);
+                //fs.WriteLine();
+                //fs.Close();
             }
         }
     }
