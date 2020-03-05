1211416b69e0e8111166dc1e3d47faa77c40e5e2
8023ljq <809395347@qq.com>
2020-03-05 17:59:10 +0800

    添加

---
+++ DapperAdmin/DapperAdminApi/App_Start/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(DapperAdminApi.App_Start.Startup))]
+
+namespace DapperAdminApi.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //消息总线--集线器Hub配置
+            app.Map("/MessageHub", map => {
+                //SignalR允许跨域调用
+                map.UseCors(CorsOptions.AllowAll);
+                HubConfiguration config = new HubConfiguration()
+                {
+                    //禁用JavaScript代理
+                    EnableJavaScriptProxies = false,
+                    //启用JSONP跨域
+                    EnableJSONP = true,
+                    //反馈结果给客户端
+                    EnableDetailedErrors = true
+                };
+                map.RunSignalR(config);
+            });
+
+            //WebApi允许跨域调用
+            app.UseCors(CorsOptions.AllowAll);
+        }
+    }
+}
---
+++ DapperAdmin/DapperAdminApi/Common/SignalR/MessageHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace DapperAdminApi.Common.SignalR
+{
+    [HubName("messagehub")]
+    public class MessageHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.addMessageToList("你好");
+        }
+
+        public void NewChatMessage(string name, string message)
+        {
+            Clients.All.addMessageToList(name, message);
+        }
+    }
+}
