6b82f674dd34c4b142c36caf6d9b17713935cebd
8023ljq <809395347@qq.com>
2020-03-05 17:53:46 +0800

    添加

---
+++ DapperAdmin/DapperSignalRHelp/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperSignalRHelp
+{
+    public class MessageHub : Hub
+    {
+        public void Welcome(string name)
+        {
+            Clients.All.listen(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + ":" + name + ":" + Context.ConnectionId);
+        }
+    }
+}
